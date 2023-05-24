09628876a2b40f0c3b6f97f8eb730a8918db015e
Dilan Nana <dilannana@DN0a1f4257.SUNet>
2023-05-24 00:53:06 -0700

    refactored to just finish and will go back to clean

--- DiscordBot/bot_copy_w_buttons.py
+++ DiscordBot/bot_copy_w_buttons.py
@@ -1,4 +1,5 @@
 # bot.py
+from typing import Optional
 import discord
 from discord.ext import commands
 import os
@@ -26,6 +27,9 @@ with open(token_path) as f:
     discord_token = tokens['discord']
 
 
+
+
+
 class ModBot(discord.Client):
     def __init__(self): 
         intents = discord.Intents.default()
@@ -34,6 +38,10 @@ class ModBot(discord.Client):
         self.group_num = None
         self.mod_channels = {} # Map from guild to the mod channel id for that guild
         self.reports = {} # Map from user IDs to the state of their report
+    
+    # def create_inner_views(self):
+    #     return ModBot.ConfirmView(self), ModBot.SelectReasonView(self)
+
 
     async def on_ready(self):
         print(f'{self.user.name} has connected to Discord! It is these guilds:')
@@ -48,20 +56,26 @@ class ModBot(discord.Client):
         else:
             raise Exception("Group number not found in bot's name. Name format should be \"Group # Bot\".")
 
+        startup_channel = None
         # Find the mod channel in each guild that this bot should report to
         for guild in self.guilds:
             for channel in guild.text_channels:
                 if channel.name == f'group-{self.group_num}-mod':
                     self.mod_channels[guild.id] = channel
-        
+                elif channel.name == f'group-{self.group_num}':
+                    startup_channel = channel
+        await startup_channel.send("WELCOME") #edit this to be pretty!
 
     async def on_message(self, message):
         '''
         This function is called whenever a message is sent in a channel that the bot can see (including DMs). 
         Currently the bot is configured to only handle messages that are sent over DMs or in your group's "group-#" channel. 
         '''
+        print("this is message = ", message)
+        print("this is message content = ", message.content)
         # Ignore messages from the bot 
         if message.author.id == self.user.id:
+            print("BOT SENT THIS MESSAGE")
             return
 
         # Check if this message was sent in a server ("guild") or if it's a DM
@@ -69,10 +83,13 @@ class ModBot(discord.Client):
             await self.handle_channel_message(message)
         else:
             await self.handle_dm(message)
+        
 
     async def handle_dm(self, message):
+       
         # Handle a help message
         if message.content == Report.HELP_KEYWORD:
+            # print("Dilan we are in trouble! ")
             reply =  "Use the `report` command to begin the reporting process.\n"
             reply += "Use the `cancel` command to cancel the report process.\n"
             await message.channel.send(reply)
@@ -90,9 +107,39 @@ class ModBot(discord.Client):
             self.reports[author_id] = Report(self)
 
         # Let the report class handle this message; forward all the messages it returns to uss
+        
         responses = await self.reports[author_id].handle_message(message)
+        print(responses)
         for r in responses:
-            await message.channel.send(r)
+            if isinstance(r, str):
+                await message.channel.send(r)
+
+        if self.reports[author_id].report_state == 0:
+            print("YOOOOOOOOO this is message = ", message)
+            view = ConfirmView()
+            await message.channel.send(view=view)
+            await view.wait()
+
+          
+            
+
+            # if view.confirmed is None:
+            #     #nothing was pressed 
+            #     logger.error("Timeout")
+            # elif view.confirmed is True:
+            #     self.on_message
+            #     logger.error("Ok")
+            # elif view.confirmed is False:
+            #     logger.error("Cancelling")
+            
+        if self.reports[author_id].report_state == 1:
+            print("CONFIRMED")
+            # view = SelectReasonView()
+            # await message.channel.send(view=view)
+            
+
+
+
 
         # If the report is complete or cancelled, remove it from our map
         if self.reports[author_id].report_complete():
@@ -101,6 +148,7 @@ class ModBot(discord.Client):
     async def handle_channel_message(self, message):
         # Only handle messages sent in the "group-#" channel
         if not message.channel.name == f'group-{self.group_num}':
+            print("this is message not in group = ", message.content)
             return
 
         # Forward the message to the mod channel
@@ -108,7 +156,11 @@ class ModBot(discord.Client):
         await mod_channel.send(f'Forwarded message:\n{message.author.name}: "{message.content}"')
         scores = self.eval_text(message.content)
         await mod_channel.send(self.code_format(scores))
-
+    
+    # @discord.Client.event
+    # async def embed(ctx):
+    #     embed = discord.Embed(title="Sample Embded", url="https://google.com", description="This is a link to Google", color="0xFF3433")
+    #     await ctx.send(embed=embed)
     
     def eval_text(self, message):
         ''''
@@ -125,7 +177,47 @@ class ModBot(discord.Client):
         shown in the mod channel. 
         '''
         return "Evaluated: '" + text+ "'"
+    
+# class SelectReasonView(discord.ui.View, ModBot):
+#         @discord.ui.button(label="Spam", style=discord.ButtonStyle.primary)
+#         async def spam(self, interaction: discord.Interaction, button: discord.ui.Button):
+#             print("Spam")
 
+#         @discord.ui.button(label="Offensive Content", style=discord.ButtonStyle.secondary)
+#         async def offensive_content(self, interaction: discord.Interaction, button: discord.ui.Button):
+#             print("Offensive")
+
+#         @discord.ui.button(label="Harassment", style=discord.ButtonStyle.secondary)
+#         async def harassment(self, interaction: discord.Interaction, button: discord.ui.Button):
+#             print("harassment")
+
+#         @discord.ui.button(label="Imminent Danger", style=discord.ButtonStyle.secondary)
+#         async def imminent_danger(self, interaction: discord.Interaction, button: discord.ui.Button):
+#             print("imminent_danger")
+
+class ConfirmView(discord.ui.View, ModBot):
+        def __init__(self, *, timeout: float | None = 180, ):
+            super().__init__(timeout=timeout)
+            self.confirmed = None
+        @discord.ui.button(label="Yes", style=discord.ButtonStyle.primary)
+        async def yes(self, interaction: discord.Interaction, button: discord.ui.Button, custom_id="yes"):
+            self.confirmed = True
+            self.clean_up()
+            await interaction.response.edit_message(view=self)
+            await interaction.followup.send("You confirmed!")
+
+        @discord.ui.button(label="No", style=discord.ButtonStyle.secondary, custom_id="no")
+        async def no(self, interaction: discord.Interaction, button: discord.ui.Button):
+            self.confirmed = False
+            self.clean_up()
+            await interaction.response.edit_message(view=self)
+            await interaction.followup.send("You confirmed!")
+        
+        def clean_up(self):
+            for x in self.children:
+                x.disabled = True
+            # button.disabled = True
+            self.stop()
 
 client = ModBot()
 client.run(discord_token)
--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -7,19 +7,32 @@ class State(Enum):
     AWAITING_MESSAGE = auto()
     MESSAGE_IDENTIFIED = auto()
     MESSAGE_CONFIRMED = auto()
+    SPAM_REPORT = auto()
+    OFF_CON_REPORT = auto()
+    HARASS_REPORT = auto()
+    IMM_DANG_REPORT = auto()
     REPORT_COMPLETE = auto()
 
 class Report:
     START_KEYWORD = "report"
     CANCEL_KEYWORD = "cancel"
     HELP_KEYWORD = "help"
-    CONFIRM_KEYWORDS ="yes, confirm"
+    MESSAGE_CONFIRMED = "yes"
+    SPAM_REASON = "spam"
+    OFF_CON_REASON = "offensive content"
+    HARASS_REASON = "harassment"
+    IMM_DANG_REASON = "imminent danger"
+    SPAM_REASONS = ["fraud/scam", "solicitation", "impersonation"]
+    OFF_CON_REASONS = ["hate speech", "sexually explicit content", "child sexual abuse material", "advocating or glorifying violence", "copyright infringement"]
+    HARASS_REASONS = ["bullying", "hate speech directed at me", "unwanted sexual content", "revealing private information"]
+    IMM_DANG_REASONS = ["self harm or suicidal intent", "credible threat of violence"]
+
 
     def __init__(self, client):
         self.state = State.REPORT_START
         self.client = client
         self.message = None
-        self.report_state = -1 #0 is start, 1 is to confirm correct message, 2 is to offer category selection
+    
     async def handle_message(self, message):
         '''
         This function makes up the meat of the user-side reporting flow. It defines how we transition between states and what 
@@ -29,17 +42,7 @@ class Report:
 
         if message.content == self.CANCEL_KEYWORD:
             self.state = State.REPORT_COMPLETE
-            print("THIS IS SELF STATE REPORT COMPLETED = ", self.state)
-            self.report_state = -1
             return ["Report cancelled."]
-
-        if message.content == self.CONFIRM_KEYWORDS:
-            print("WE ARE HERE IN LINE 35")
-            reply =  "Thank you for confirming! "
-            reply += "Please select the reason for reporting this message\n"
-            self.state = State.MESSAGE_CONFIRMED
-            self.report_state = 1 #confirmed
-            return [reply, {"report_state": self.report_state} ]
         
         if self.state == State.REPORT_START:
             reply =  "Thank you for starting the reporting process. "
@@ -67,21 +70,80 @@ class Report:
 
             # Here we've found the message - it's up to you to decide what to do next!
             self.state = State.MESSAGE_IDENTIFIED
-            self.report_state = 0 #0 = awaiting confirmation, 1-10 etc. mean different renderings. 
-
-            # return ["I found this message:", "```" + message.author.name + ": " + message.content + "```", \
-            #         "This is all I know how to do right now - it's up to you to build out the rest of my reporting flow!"]
+            return ["I found this message:", "```" + message.author.name + ": " + message.content + "```", \
+                    "Is this the correct message? Type 'yes' to continue or type 'cancel' to restart."]
         
         if self.state == State.MESSAGE_IDENTIFIED:
-            return ["I found this message:", "```" + message.author.name + ": " + message.content + "```" + "Is this the message you are wishing to report? \nClick Yes to continue or type 'cancel' to enter a new link.", {"report_state": self.report_state}]
-            # button = discord.ui.Button(label="Click me")
-            # view = discord.ui.View()
-            # view.add_item(button)
-            # return [view]
-            
-            
+            #handle here for words that we do not expect, set self.state = State.REPORT_COMPLETE return ["Report cancelled."]
+            if message.content == 'yes' or message.content == 'Yes' or message.content == 'YES':
+                self.state = State.MESSAGE_CONFIRMED
+                return ["Please type the reason for reporting this message from the following list:", \
+                    "'Spam', 'Offensive Content', 'Harassment', 'Imminent Danger' or type 'cancel' to restart"]
+            #else try again?
+            else:
+                return ["Sorry! It looks like that was an invalid input.", \
+                        "Please type the reason for reporting this message from the following list:", \
+                        "'Spam', 'Offensive Content', 'Harassment', 'Imminent Danger' or type 'cancel' to restart"]
 
+        if self.state == State.MESSAGE_CONFIRMED:
+            #they've confirmed and typed in a reason 
+            user_msg = message.content
+            user_msg = user_msg.lower()
+            reasons = [self.SPAM_REASON, self.OFF_CON_REASON, self.HARASS_REASON, self.IMM_DANG_REASON]
+            if user_msg in reasons:
+                if user_msg == 'spam':
+                    print("FOUND SPAM")
+                    self.state = State.SPAM_REPORT
+                    return ["Please select the type of " + user_msg]
+                elif user_msg == 'offensive content':
+                    self.state = State.OFF_CON_REPORT
+                    return ["Please select the type of " + user_msg, \
+                            "Type: 'Hate Speech', 'Sexually explicit content', 'Child sexual abuse material', 'Advocating or glorifying violence', 'Copyright infringement', or type 'cancel' to restart"
+                            ]
+                elif user_msg == 'harassment':
+                    self.state = State.HARASS_REPORT
+                    return ["Please select the type of " + user_msg]
+                elif user_msg == 'imminent danger':
+                    self.state = State.IMM_DANG_REPORT
+                    return ["Please select the type of " + user_msg]
+            else:
+                return ["Oops"]
+               
+        if self.state == State.SPAM_REPORT:
+            user_msg = message.content
+            user_msg = user_msg.lower()
+            if user_msg in self.SPAM_REASONS:
+                #add in error handling
+                spam_reason = next((reason for reason in self.SPAM_REASONS if user_msg == reason), ["error"])
+                return ["Thank you for reporting a potential instance of " + spam_reason + ". Our content moderation team will review the message(s) and decide on appropriate action. This may include post and/or account removal."]
+        if self.state == State.OFF_CON_REPORT:
+            user_msg = message.content
+            user_msg = user_msg.lower()
+            if user_msg in self.OFF_CON_REASONS:
+                off_con_reason = next((reason for reason in self.OFF_CON_REASONS if user_msg == reason), ["error"])
+                return ["Thank you for reporting a potential instance of " + off_con_reason + ". Our content moderation team will review the message(s) and decide on appropriate action. This may include post and/or account removal."]
+        if self.state == State.HARASS_REPORT:
+            user_msg = message.content
+            user_msg = user_msg.lower()
+            if user_msg in self.HARASS_REASONS:
+                harass_reason = next((reason for reason in self.HARASS_REASONS if user_msg == reason), ["error"])
+                return ["Thank you for reporting a potential instance of " + harass_reason + ". Our content moderation team will review the message(s) and decide on appropriate action. This may include post and/or account removal."]
+        if self.state == State.IMM_DANG_REPORT:
+            user_msg = message.content
+            user_msg = user_msg.lower()
+            if user_msg in self.IMM_DANG_REASONS:
+                imm_dang_reason = next((reason for reason in self.IMM_DANG_REASONS if user_msg == reason), ["error"])
+                return ["Thank you for reporting a potential instance of " + imm_dang_reason + ". Our content moderation team will review the message(s) and decide on appropriate action. This may include post and/or account removal."]
+        
+
+        # if self.state == State.MESSAGE_REASON_IDENTIFIED:
+        #     return 
         return []
+
+    def format_message(message):
+        user_msg = message.content
+        user_msg = user_msg.lower()
+        return user_msg
 
     def report_complete(self):
         return self.state == State.REPORT_COMPLETE
@@ -89,4 +151,3 @@ class Report:
 
 
     
-
